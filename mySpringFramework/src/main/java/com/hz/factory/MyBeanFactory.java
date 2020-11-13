2c43a897c8986c228fa156e8264199a63410f761
hz <869932173@qq.com>
2020-11-13 10:39:56 +0800

    spring官方文档 第一节 ioc container

--- mySpringFramework/src/main/java/com/hz/factory/MyBeanFactory.java
+++ mySpringFramework/src/main/java/com/hz/factory/MyBeanFactory.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 /**
+ * BeanFactoryPostProcessor 可以插手bean的定义 beanDefiniton
+ * 此时的spring容器还没有开始实例化bean，
+ * 因此自定义的BeanFactoryPostProcessor实现类不要做与bean实例有关的操作，
+ * 而是做一些与bean定义有关的操作，例如修改某些字段的值，这样后面实例化的bean的就会有相应的改变；
  * @author hz
  * @create 2020-10-09
  */
@@ -15,7 +19,6 @@ public class MyBeanFactory implements BeanFactoryPostProcessor {
 		System.out.println("my bean factory start.....");
 		//通过使用BeanFactory.getBean())，但是这样做会导致过早的bean实例化，违反标准的容器生命周期。
 		// 这可能会导致负面的副作用，比如绕过bean的后处理。
-
 		//如果希望更改实际bean实例(即从配置元数据创建的对象)，
 		// 则需要使用BeanPostProcessor(在前面通过使用BeanPostProcessor自定义bean中进行了描述)。
 //		BeanDefinition userDaoBD = beanFactory.getBeanDefinition("userDao");
