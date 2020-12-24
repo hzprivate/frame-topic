c63bb22f096e193911f69a35e02b51614b159ca4
hz <869932173@qq.com>
2020-12-24 22:01:01 +0800

    模拟springboot ，springboot源码分析

---
+++ simulate-springboot/src/main/java/MyApplicationContext.java
@@ -0,0 +1,11 @@
+import com.hz.TestTomcat;
+
+/**
+ * @author hz
+ * @create 2020-12-24
+ */
+public class MyApplicationContext {
+    public static void main(String[] args) {
+        TestTomcat.run();
+    }
+}
