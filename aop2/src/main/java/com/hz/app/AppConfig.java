ae4ea8070a6b80ad884f61e9f5b519bf73cf0184
hz <869932173@qq.com>
2021-01-21 16:22:30 +0800

    对注解的深入使用

--- aop2/src/main/java/com/hz/app/AppConfig.java
+++ aop2/src/main/java/com/hz/app/AppConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @ComponentScan("com.hz")
 //在注解上定义属性，容器初始化时赋值给某个对象
-@AutoSetValueOnRefresh(ip="192.168.20.185",address = "浙江宁波")
+@AutoSetValueOnRefresh(ip="112.158.22.155",address = "浙江宁波")
 //实现通过注解开启HzConfig类的bean
 @EnableHzConfig
 public class AppConfig {
